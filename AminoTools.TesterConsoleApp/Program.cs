7dc97466c9822c36d3515ce7a7dff30ac284c05d
Elertan <denkievits@gmail.com>
2017-08-29 18:32:51 +0200

    Added tester console project

---
+++ AminoTools.TesterConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AminoApi;
+
+namespace AminoTools.TesterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync();
+            Console.ReadKey();
+        }
+
+        private static async void MainAsync()
+        {
+            var api = new Api(new HttpClient());
+            var loginResult = await api.Global.S.Auth.Login("denkievits@gmail.com", "4dpwpagvw");
+
+
+        }
+    }
+}
